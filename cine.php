ca852136d3a1d227e4d57798708dcb7281b1165c
Maria Jose <marijopereira390@gmail.com>
2020-11-15 18:01:36 -0600

    Modificacion de pagina principal (cine.php)

--- cine.php
+++ cine.php
@@ -1,15 +1,3 @@
-
-<?php
-include 'connDB.php';
-$abrirCon = OpenCon();
-
-CloseCon($abrirCon);
-
-?> 
-
-
-
-
 <!doctype html>
 <html lang="en">
 
@@ -22,81 +10,137 @@ CloseCon($abrirCon);
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
     integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 <link rel="stylesheet" href="principal.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
   <title>Principal</title>
 </head>
+<body>
 
-<body >
-  <form action="">
-  
 
+<!-- Navbar (sit on top) -->
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-wide w3-padding w3-card">
+    <a href="#home" class="w3-bar-item w3-button"><b>CINEPLUS</a>
+    <!-- Float links to the right. Hide them on small screens -->
+    <div class="w3-right w3-hide-small">
+      <a href="" class="w3-bar-item w3-button">Cartelera</a>
+      <a href="" class="w3-bar-item w3-button">Sucursales</a>
+      <a href="login.php" class="w3-bar-item w3-button">Iniciar Sesión</a>
     </div>
+  </div>
+</div>
 
-    <div class="container-fluid">
-      <div id="page-content-wrapper justify-content-md-end">
 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom ">
-          <button class="btn btn-danger" id="menu-toggle">Menu</button>
 
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+<!-- Page content -->
+<div class="w3-content w3-padding" style="max-width:1564px">
 
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-              <li class="nav-item">
-                <?php
-                if(isset($_GET['user']))
-                  {	
-                    $usuario = $_GET['user'];
-                    echo '<p style="padding: 0.5rem; margin: 0;"> Hola '. $usuario .'!</p>';
-                  }
-                else {
-                  echo '<a class="nav-link" href="login.php">SIGN IN</a>';
-                }
-                ?> 
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
-              </li>
-              <!-- <li class="nav-item">
-                <a class="nav-link" href="login.html">SIGN IN</a>
-              </li> -->
+  <!-- Project Section -->
+  <div class="w3-container w3-padding-32" id="projects">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Películas</h3>
+  </div>
 
-              <li>
-              </li>
-            </ul>
-          </div>
-        </nav>
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Mulan</div>
+        <img src="/proyecto/Cine_web/media/Mulan.jpg" alt="House" style="width:100%">
       </div>
     </div>
-    <div class="container-fluid">
-      <div class="row ">
-        <div class="col-2">
-          <div class="d-flex" id="wrapper">
-            <div class="bg-light border-right" id="sidebar-wrapper">
-              <div class="sidebar-heading">CINEPLUS</div>
-               <div class="list-group list-group-flush">
-                <?php include 'menu.php';?>
-               </div>
-              
-          </div>
-         
-          </div>
-        </div>
-     
-        <div class="col-10.">
-       
-      
-        </div>
-        
-        </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Tenet</div>
+        <img src="/proyecto/Cine_web/media/Tenet.jpg" alt="House" style="width:100%">
       </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Wonder Woman 1984</div>
+        <img src="/proyecto/Cine_web/media/WonderWoman.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Sherlock Holmes 3</div>
+        <img src="/proyecto/Cine_web/media/Sherlock.jpg" alt="House" style="width:100%">
+      </div>
+    </div>
+  </div>
 
-  </form>
+  <div class="w3-row-padding">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">Black Widow</div>
+        <img src="/proyecto/Cine_web/media/BlackWidow.jpg" alt="House" style="width:99%">
+      </div>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <div class="w3-display-container">
+        <div class="w3-display-topleft w3-black w3-padding">The Conjuring</div>
+        <img src="/proyecto/Cine_web/media/Conjuring.jpg" alt="House" style="width:99%">
+      </div>
+    </div>
+  </div>
 
+  <!-- About Section -->
+  <div class="w3-container w3-padding-32" id="about">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Sobre Nosotros</h3>
+    <p>Somos CINEPLUS y nuestro propósito es hacer sentir a los clientes bastante cómodos
+    para que tengan una experiencia increíble tanto en la página web como en el establecimiento. Nuestro emprendimiento
+    nace como fruto de un proyecto universitario.
+    </p>
+  </div>
+
+  <div class="w3-row-padding w3-grayscale">
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="/proyecto/Cine_web/media/Omar.png"  style="width:100%">
+      <h3>Omar Herrera</h3>
+      <p class="w3-opacity">CEO & Fundador</p>
+      <p><button class="w3-button w3-light-grey w3-block">Contactar</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="/proyecto/Cine_web/media/Girls.png"  style="width:100%">
+      <h3>Nathalia Hernandez</h3>
+      <p class="w3-opacity">CEO & Fundadora</p>
+      <p><button class="w3-button w3-light-grey w3-block">Contactar</button></p>
+    </div>
+    <div class="w3-col l3 m6 w3-margin-bottom">
+      <img src="/proyecto/Cine_web/media/Girls.png"  style="width:100%">
+      <h3>Maria Jose Uribe</h3>
+      <p class="w3-opacity">CEO & Fundadora</p>
+      <p><button class="w3-button w3-light-grey w3-block">Contactar</button></p>
+    </div>
+    
+  </div>
+
+  <!-- Contact Section -->
+  <div class="w3-container w3-padding-32" id="contact">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Contáctenos</h3>
+    <p>Completa los siguientes espacios para saber de ti.</p>
+    <form action="/action_page.php" target="_blank">
+      <input class="w3-input w3-border" type="text" placeholder="Nombre" required name="Nombre">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Correo" required name="Correo">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Tema" required name="Tema">
+      <input class="w3-input w3-section w3-border" type="text" placeholder="Comentario" required name="Comentario">
+      <button class="w3-button w3-black w3-section" type="submit">
+        <i class="fa fa-paper-plane"></i> ENVIAR
+      </button>
+    </form>
+  </div>
   
-  <!-- Optional JavaScript; choose one of the two! -->
+<!-- Image of location/map -->
+<div class="w3-container">
+  <img src="/proyecto/Cine_web/img/CINEPLUS.jpg" class="w3-image" style="width:700px" >
+</div>
+
+<!-- End page content -->
+</div>
+
+
+<!-- Footer -->
+<footer class="w3-center w3-black w3-padding-16">
+  <p>Gracias por preferirnos!</a></p>
+</footer>
+<!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
@@ -112,5 +156,6 @@ CloseCon($abrirCon);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
     -->
 </body>
+
 
 </html>
--- menu.php
+++ menu.php
@@ -1,7 +1,7 @@
 <?php
 echo'
         <a href="#" class="list-group-item list-group-item-action bg-light">Películas</a>
-        <a href="#" class="list-group-item list-group-item-action bg-light">Cartelera</a>
+        <a href="cartelera.php" class="list-group-item list-group-item-action bg-light">Cartelera</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Sucursales</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Contáctenos</a>
         <a href="#" class="list-group-item list-group-item-action bg-light">Sobre CINEPLUS</a>
